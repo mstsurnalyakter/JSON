53dcf789fe19b15a77d82b678a85b482e7b679aa
mstsurnalyakter <surnalyms@gmail.com>
2024-02-26 19:00:06 +0600

    theory

---
+++ index1.js
@@ -0,0 +1,55 @@
+/**
+ * Why JSON?
+ *
+ * What is JSON?
+ *
+ * **** JavaScript Object Notation(JavaScript Syntax)******
+ * **** JSON ->
+ * ****** -> A standard
+ * ****** -> Text based form,
+ * ****** -> Human readable form,
+ * ****** -> Lightweight,
+ * ****** -> Language independent
+ * ****** -> to store and exchange data between client and server
+ * ****** -> JSON File extension --> .json
+ * ****** -> Official media --> application/json
+ *
+ * JSON Basic Structure
+ *
+ * *********  Object vs  Array ***************
+ * ****** -> Object
+ * ****** -> {
+ *              'key':'value',
+ *              'key':'value'
+ *
+ *              }
+ * ****** -> object are separated by comma
+ *
+ * * ****** -> Array [value1, value2]
+ *
+ *
+ * JSON Syntax
+ * * ****** -> JSON Syntax [key must be double quote] [with [] brace]
+ *               [
+ *                  {
+ *                      "key":"value"
+ *                       "key":"value"
+ *                  }
+ *                 ]
+ *
+ *
+ * JSON Data Type
+ * ***************** JSON Data Type **********************
+ * ****** -> String
+ * ****** -> Number(Int/float)
+ * ****** -> Boolean
+ * ****** -> Null
+ * ****** -> Object
+ * ****** -> Array
+ *
+ *
+ * JavaScript Object VS JSON
+ *
+ *
+ * 
+ */
