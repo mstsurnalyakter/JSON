c8b14fb182e7ab1fabb26d737b37874506b97e1c
mstsurnalyakter <surnalyms@gmail.com>
2024-02-27 12:25:32 +0600

    async await

--- module/post.js
+++ module/post.js
@@ -24,3 +24,42 @@ const displayPost = posts =>{
 const handleError = err =>{
     alert(`Error Status: ${err.status} && Message: ${err.message} `)
 }
+
+const deletePost = () =>{
+    fetch("https://jsonplaceholder.typicode.com/posts/1",{
+        method:'DELETE'
+    });
+}
+
+const updatePost = () =>{
+    fetch("https://jsonplaceholder.typicode.com/posts/1", {
+      method: "PUT",
+      body: JSON.stringify({
+        id: 1,
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+}
+
+const createAPost = () =>{
+    fetch("https://jsonplaceholder.typicode.com/posts", {
+      method: "POST",
+      body: JSON.stringify({
+        title: "foo",
+        body: "bar",
+        userId: 1,
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      .then((response) => response.json())
+      .then((json) => console.log(json));
+}
